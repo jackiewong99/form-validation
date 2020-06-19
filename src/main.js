0a0e9dbecafec8096e9f7c17c170a1e46f2cf987
Jackie Wong <wongjackie888@gmail.com>
2020-06-18 19:13:19 -1000

    Add vue-router.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Home from './views/Home';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('./views/About.vue')
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
