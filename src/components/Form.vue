5a0ea04f6f983118a5a8aad7d3cbe9dd16ad57a1
Jackie Wong <wongjackie888@gmail.com>
2020-06-18 19:19:38 -1000

    Create form, add Inputs component, and style form.

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form">
+    <form class="form-container">
+      <Inputs />
+    </form>
+  </div>
+</template>
+
+<script>
+import Inputs from './Inputs';
+export default {
+  name: 'Form',
+  components: {
+    Inputs
+  }
+};
+</script>
+
+<style scoped>
+.form {
+  border-radius: 10px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
+</style>
