741b951a9f17ee7f82e1e60da3169d961dddb188
Jackie Wong <wongjackie888@gmail.com>
2020-06-18 19:22:25 -1000

    Create About component and add temporary text.

---
+++ src/views/About.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe
+      molestiae delectus aut alias? Provident doloribus laboriosam ut nisi
+      deleniti perspiciatis sit veritatis velit quis eius atque tempore vel,
+      voluptatum, quae enim quisquam obcaecati! Ut recusandae quam
+      exercitationem aperiam. Libero laboriosam dolorum perferendis itaque
+      numquam officiis molestias facere cum dolor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe
+      molestiae delectus aut alias? Provident doloribus laboriosam ut nisi
+      deleniti perspiciatis sit veritatis velit quis eius atque tempore vel,
+      voluptatum, quae enim quisquam obcaecati! Ut recusandae quam
+      exercitationem aperiam. Libero laboriosam dolorum perferendis itaque
+      numquam officiis molestias facere cum dolor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo saepe
+      molestiae delectus aut alias? Provident doloribus laboriosam ut nisi
+      deleniti perspiciatis sit veritatis velit quis eius atque tempore vel,
+      voluptatum, quae enim quisquam obcaecati! Ut recusandae quam
+      exercitationem aperiam. Libero laboriosam dolorum perferendis itaque
+      numquam officiis molestias facere cum dolor.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+};
+</script>
+
+<style scoped></style>
