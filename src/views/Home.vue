bd93f8786ad35051aae82c916f95b15a07a7a8bd
Jackie Wong <wongjackie888@gmail.com>
2020-06-18 19:23:17 -1000

    Create Home component, add Form component, and style elements.

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Form Validation</h1>
+    <Form id="form" />
+  </div>
+</template>
+
+<script>
+import Form from '../components/Form';
+
+export default {
+  name: 'Home',
+  components: {
+    Form
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+#form {
+  width: 50%;
+}
+</style>
