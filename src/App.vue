9504dda88922d9349e9b0792dcb192a1de751f3a
Jackie Wong <wongjackie888@gmail.com>
2020-06-18 19:17:11 -1000

    Update Header component, add router, and style App component.
    -Create grid layout for App component.
    -Reposition header
    -Position each view in App component.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <Header />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header id="nav" />
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    Header,
-    HelloWorld
+    Header
   }
 };
 </script>
@@ -26,8 +24,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
+  height: 100%;
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-auto-rows: 62px;
+}
+
+#nav {
+  grid-row: 1 / 1;
+  align-self: center;
+  justify-self: end;
+  margin-right: 15%;
+}
+
+.view {
+  grid-row: 2 / 11;
+  justify-self: center;
+  width: 50%;
 }
 </style>
