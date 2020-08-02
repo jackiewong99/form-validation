5ab6d84df12d39c72ca1fa76b1b206207cb315b3
Jackie Wong <wongjackie888@gmail.com>
2020-08-01 16:32:52 -1000

    Create successful sign up message view.
    Move to the 'views' folder.

---
+++ src/views/SignUpMsg.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="signup-message">
+    <h1>
+      Successfully signed up!
+    </h1>
+    <h4>Thank you for trying out my form validation project!</h4>
+    <div>
+      <p>
+        A verification email has been sent. Follow the link in the email to
+        verify your email.
+      </p>
+      <p>
+        <em
+          >NOTE: There is no login functionality, your account will be deleted
+          after verification, and the information you have entered will not be
+          used or sold for any purposes.</em
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUpMsg'
+};
+</script>
+
+<style scoped></style>
