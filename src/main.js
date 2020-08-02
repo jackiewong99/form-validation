40054128d8c2278e956e76c0e115d45e47293f4b
Jackie Wong <wongjackie888@gmail.com>
2020-08-01 20:15:48 -1000

    Add route for successful sign up message.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import { firestorePlugin } from 'vuefire';
 import App from './App.vue';
 import Home from './views/Home';
+import SignUpMsg from './views/SignUpMsg';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
@@ -18,6 +19,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: () => import('./views/About.vue')
+  },
+  {
+    path: '/success',
+    name: 'signup',
+    component: SignUpMsg
   }
 ];
 
